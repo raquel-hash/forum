d2c43c860a0a5a69108a87f9a1db1a43a80e7295
Raquel <quelcaraquel1@gmail.com>
2024-06-20 16:37:07 -0400

    Filtrado por campo de búsqueda

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -8,13 +8,17 @@ use Livewire\Component;
 
 class ShowThreads extends Component
 {
+    public $search = '';
     public function render()
     {
         $categories = Category::get();
-        $threads = Thread::latest()->withCount('replies')->get();
+        $threads = Thread::query();
+        $threads->where('title', 'like', "%$this->search%");
+        $threads->withCount('replies');
+        $threads->latest();
         return view('livewire.show-threads', [
             'categories' => $categories,
-            'threads' => $threads
+            'threads' => $threads->get(),
         ]);
     }
 }
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -23,6 +23,10 @@
     </div>
     <div class="w-full">
         <!--Formulario-->
+        <form action="" class="mb-4">
+            <input type="text" placeholder="// ..."
+                class="bg-slate-800 border-0 rounded-md w-1/3 p-3 text-white/60 text-xs" wire:model.live="search">
+        </form>
         @foreach ($threads as $thread)
             <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
                 <div class="p-4 flex gap-4">
