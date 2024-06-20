dea104e6cb28ecbaa79071b9cf5276b782aa5c09
Raquel <quelcaraquel1@gmail.com>
2024-06-20 18:02:25 -0400

    Crear una respuesta principal

--- app/Livewire/ShowThread.php
+++ app/Livewire/ShowThread.php
@@ -8,6 +8,20 @@ use Livewire\Component;
 class ShowThread extends Component
 {
     public Thread $thread;
+    public $body = '';
+
+    public function postReply()
+    {
+        // validate
+        $this->validate(['body' => 'required']);
+        //create
+        auth()->user()->replies()->create([
+            'thread_id' => $this->thread->id,
+            'body' => $this->body
+        ]);
+        //refresh
+        $this->body = '';
+    }
     public function render()
     {
         return view('livewire.show-thread');
