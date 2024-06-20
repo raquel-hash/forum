2da7bb00d810bc45a03966b29c7020a1418c4583
Raquel <quelcaraquel1@gmail.com>
2024-06-20 14:50:54 -0400

    Tabla categorías

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->create(['email'=>'r@app.com']);
+        User::factory(9)->create();
+        Category::factory(10)->create();
     }
 }
