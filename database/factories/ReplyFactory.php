b2fbfb90b5b76999733a3ed4f4336c5a65976f4a
Raquel <quelcaraquel1@gmail.com>
2024-06-20 15:28:18 -0400

    Tabla respuestas

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use function Laravel\Prompts\text;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reply>
+ */
+class ReplyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'thread_id' => rand(1, 200),
+            'user_id' => rand(1, 10),
+            'body' => fake()->text(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Reply;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,7 @@ class DatabaseSeeder extends Seeder
         Category::factory(10)
             ->hasThreads(20)
             ->create();
+
+        Reply::factory(400)->create();
     }
 }
